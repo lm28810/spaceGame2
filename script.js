78a7de7d7a52a8e161134a01d343d18939e14774
Jazmine <lm28810@gmail.com>
2022-12-15 14:13:14 -0500

    css almost done

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let darkicon = document.querySelector('.icon');
+darkicon.addEventListener('click', function () {
+    let body = document.body;
+    body.classList.toggle('dark-mode')
+})
+console.log(darkicon)
